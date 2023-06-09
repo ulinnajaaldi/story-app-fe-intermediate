8efe1a69c15b697da330c44b9a400f6d6af23d68
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-06-09 23:10:11 +0700

    feat: create configuration for HtmlWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,25 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const htmlWebpackPluginConfig = {
+  meta: {
+    viewport:
+      'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
+  },
+  templateParameters: {
+    brandName: 'Ceritain!',
+  },
+};
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, "src/js/index.js"),
+    app: path.resolve(__dirname, 'src/js/index.js'),
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
@@ -17,21 +28,33 @@ module.exports = {
         test: /\.(s[ac]ss)$/i,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: () => [require("autoprefixer")],
+                plugins: () => [require('autoprefixer')],
               },
             },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
           },
         ],
       },
@@ -39,11 +62,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Dashboard",
-      filename: "index.html",
-      template: path.resolve(__dirname, "src/views/index.html"),
+      title: 'Ceritain! - Home',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/views/index.html'),
+      ...htmlWebpackPluginConfig,
     }),
-
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
     new CleanWebpackPlugin(),
   ],
 };
