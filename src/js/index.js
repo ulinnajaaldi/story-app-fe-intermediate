45d420b764923903902baa3863ae6b57a40deb26
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-06-11 00:53:44 +0700

    feat: create new routes & pages structure validation

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,30 @@ import './components/index';
 // Import javascript file as needed
 import * as bootstrap from 'bootstrap';
 import Home from './pages/home';
+import Dashboard from './pages/dashboard';
+import AddStory from './pages/add-story';
 
 const routes = {
   '/': Home,
-  //   '/user/tambah-cerita': 'About',
-  //   '/user/dashboard': 'Contact',
+  '/user/dashboard.html': Dashboard,
+  '/user/add-story.html': AddStory,
 };
 
 const detectRoute = () => routes[window.location.pathname];
 
+const initPages = () => {
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+
+  if (header && main && footer) {
+    main.style.minHeight = `calc(80vh - ${header.clientHeight + footer.clientHeight}px)`;
+  }
+};
+
 window.addEventListener('DOMContentLoaded', async () => {
+  initPages();
+
   const route = detectRoute();
   route.init();
 });
