b0f19a02ee49a7e5aee54ee31e58f09ff6e74bab
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-06-13 21:12:33 +0700

    feat: add content and functionality for the add-story

--- src/js/pages/add-story.js
+++ src/js/pages/add-story.js
@@ -1,3 +1,69 @@
-const AddStory = { async init() {} };
+import { render } from 'lit';
+
+const AddStory = {
+  async init() {
+    this._initialListener();
+  },
+
+  _initialListener() {
+    const evidenceInput = document.querySelector('#validationCustomEvidence');
+    evidenceInput.addEventListener('change', () => {
+      this._updatePhotoPreview();
+    });
+
+    const addStoryForm = document.querySelector('#add-story-form');
+    addStoryForm.addEventListener(
+      'submit',
+      (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        addStoryForm.classList.add('was-validated');
+        this._addStory();
+      },
+      false,
+    );
+  },
+
+  _addStory() {
+    const storyData = this._getStoryData();
+    if (this._validateStoryData({ ...storyData })) {
+      console.log('storyData');
+      console.log(storyData);
+    }
+  },
+
+  _getStoryData() {
+    const storyInput = document.querySelector('#validationCustomStory');
+    const evidenceInput = document.querySelector('#validationCustomEvidence');
+    return {
+      story: storyInput.value,
+      evidence: evidenceInput.files[0],
+      date: new Date().toLocaleDateString(),
+    };
+  },
+
+  _updatePhotoPreview() {
+    const evidenceImg = document.querySelector('#validationCustomEvidenceImg');
+    const evidenceImgInput = document.querySelector('#validationCustomEvidence');
+
+    const photo = evidenceImgInput.files[0];
+    if (!photo) return;
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      evidenceImg.parentElement.classList.remove('d-none');
+      evidenceImg.src = event.target.result;
+    };
+
+    reader.readAsDataURL(photo);
+  },
+
+  _validateStoryData(storyData) {
+    const storyDataFiltered = Object.values(storyData).filter((item) => item === '');
+
+    return storyDataFiltered.length === 0;
+  },
+};
 
 export default AddStory;
