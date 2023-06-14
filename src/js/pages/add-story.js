48f83af8c7cd7391bc535acdee8f2538b910daea
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-06-15 00:27:08 +0700

    refactor: change the HTML form with form validation components

--- src/js/pages/add-story.js
+++ src/js/pages/add-story.js
@@ -1,16 +1,9 @@
-import { render } from 'lit';
-
 const AddStory = {
   async init() {
     this._initialListener();
   },
 
   _initialListener() {
-    const evidenceInput = document.querySelector('#validationCustomEvidence');
-    evidenceInput.addEventListener('change', () => {
-      this._updatePhotoPreview();
-    });
-
     const addStoryForm = document.querySelector('#add-story-form');
     addStoryForm.addEventListener(
       'submit',
@@ -41,22 +34,6 @@ const AddStory = {
       evidence: evidenceInput.files[0],
       date: new Date().toLocaleDateString(),
     };
-  },
-
-  _updatePhotoPreview() {
-    const evidenceImg = document.querySelector('#validationCustomEvidenceImg');
-    const evidenceImgInput = document.querySelector('#validationCustomEvidence');
-
-    const photo = evidenceImgInput.files[0];
-    if (!photo) return;
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      evidenceImg.parentElement.classList.remove('d-none');
-      evidenceImg.src = event.target.result;
-    };
-
-    reader.readAsDataURL(photo);
   },
 
   _validateStoryData(storyData) {
