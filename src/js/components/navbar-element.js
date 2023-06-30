66473bec87f5d4196e707a69231501eac118593a
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-01 00:14:48 +0700

    feat: improving the functionality of the navbar items

--- src/js/components/navbar-element.js
+++ src/js/components/navbar-element.js
@@ -1,6 +1,8 @@
 import { html } from 'lit';
 import LitWithoutShadowDom from './base/lit-without-shadow-dom';
 import { msg, updateWhenLocaleChanges } from '@lit/localize';
+import Utils from '../utils/utils';
+import CheckUserAuth from '../pages/auth/check-user-auth';
 
 class NavbarElement extends LitWithoutShadowDom {
   static properties = {
@@ -20,6 +22,8 @@ class NavbarElement extends LitWithoutShadowDom {
   }
 
   render() {
+    const userName = Utils.getUserName();
+
     window.addEventListener('scroll', () => {
       const navbar = document.querySelector('nav');
       if (window.scrollY > 0) {
@@ -61,11 +65,36 @@ class NavbarElement extends LitWithoutShadowDom {
             <div class="offcanvas-body">
               <ul class="navbar-nav justify-content-end flex-grow-1 gap-3 align-items-center pe-3">
                 <nav-link to="/" content="${msg(`Beranda`)}"></nav-link>
-                <nav-link to="/user/add-story.html" content="${msg(`Tambah Ceritamu`)}"></nav-link>
+                <nav-link to="/user/add-story.html" content="${msg(`Tambah Ceritamu`)}"> </nav-link>
                 <nav-link-button
-                  to="/user/dashboard.html"
-                  content="${msg(`Dasbor`)}"
+                  to="/auth/login.html"
+                  id="loginMenu"
+                  content="${msg(`Masuk`)}"
                 ></nav-link-button>
+                <li class="nav-item dropdown d-none" id="userLogMenu">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    id="navabarUserLogMenu"
+                  >
+                    ${userName}
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a aria-current="page" class="dropdown-item" href="/user/dashboard.html"
+                        >${msg(`Dasbor`)}</a
+                      >
+                    </li>
+                    <li>
+                      <button class="dropdown-item text-danger" @click=${this._userLogOut}>
+                        ${msg(`Keluar`)}
+                      </button>
+                    </li>
+                  </ul>
+                </li>
                 <nav-locale-picker class="nav-item"></nav-locale-picker>
               </ul>
             </div>
@@ -73,6 +102,13 @@ class NavbarElement extends LitWithoutShadowDom {
         </div>
       </nav>
     `;
+  }
+
+  _userLogOut(event) {
+    event.preventDefault();
+    Utils.destroyUserToken();
+
+    CheckUserAuth.checkLoginState();
   }
 }
 
