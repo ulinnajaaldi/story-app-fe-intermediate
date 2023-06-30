e2bf7a516a2d7c8b0a525f6b3492ce796c513aee
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-01 00:12:58 +0700

    chore: adding several new components

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -5,10 +5,17 @@ import './navbar/nav-link';
 import './navbar/nav-link-button';
 import './navbar-element';
 
+import './alert/alert-success-register';
+import './alert/alert-error-register';
+import './alert/alert-error-login';
+
 import './card/card-story-dashboard';
 import './profile-card-element';
+import './card/card-no-story';
 
 import './button/button-share-story';
+import './button/auth-login-button';
+import './button/auth-register-button';
 
 import './label/label-input-image';
 import './label/label-textarea';
@@ -16,7 +23,16 @@ import './label/label-textarea';
 import './form/textarea-with-validatiton';
 import './form/input-image-with-preview';
 import './form/form-title-add-story';
+import './form/form-title-add-guest-story';
 import './form/form-title-edit-story';
+import './form/input-with-validation';
+import './form/input-password-with-validation';
 
 import './load-more-element';
 import './navbar/nav-locale-picker';
+
+import './login-header-element';
+import './register-header-element';
+
+import './modal/detail-modal';
+import './location-permission';
