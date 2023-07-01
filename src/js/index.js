8cc4540ef165188f9863c0a4db978c62f5046d31
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-01 17:14:53 +0700

    feat: adding Firebase dependencie

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
+/* eslint-disable import/first */
+/* eslint-disable import/no-extraneous-dependencies */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGTFiSNYGy4ADAUXhPZUm_wKigybjvSCU',
+  authDomain: 'ceritain-app.firebaseapp.com',
+  projectId: 'ceritain-app',
+  storageBucket: 'ceritain-app.appspot.com',
+  messagingSenderId: '535803200057',
+  appId: '1:535803200057:web:c26801c6539de8b746c3e8',
+  measurementId: 'G-Z1M8DBKG2W',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Import our custom CSS
 import '../scss/main.scss';
 
